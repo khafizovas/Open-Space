65b0194a7361699bd4b93bc06b2fa9d573832533
khafizovas <justhomosapiens121@gmail.com>
2021-09-11 03:16:48 +0300

    add disableInputs function

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function handleLoad(e) {
     disableInputs(e, inputs.slice(2));
     inputs[1].addEventListener('click', () => handleUnlock(e, inputs[0].value, inputs.slice(2)));
 }
+
+function disableInputs(e, inputs) {
+    inputs.forEach(input => input.setAttribute('disabled', 'disabled'));
+}
+
