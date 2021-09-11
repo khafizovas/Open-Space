6497b280624cd3efa7746f4f2085cb27ad3fdc96
khafizovas <justhomosapiens121@gmail.com>
2021-09-11 03:16:27 +0300

    add load event handler

---
+++ index.js
@@ -0,0 +1,10 @@
+const CORRECT_PASSWORD = 'TrustNo1';
+
+document.addEventListener('DOMContentLoaded', handleLoad);
+
+function handleLoad(e) {
+    const inputs = [...document.querySelectorAll('input')];
+
+    disableInputs(e, inputs.slice(2));
+    inputs[1].addEventListener('click', () => handleUnlock(e, inputs[0].value, inputs.slice(2)));
+}
